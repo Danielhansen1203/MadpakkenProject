3281f2c15064cacbb36afad15eab2cbf11cf5163
Daniel Hansen <danielhansen1203@msn.com>
2016-11-17 14:32:26 +0100

    Menu knap for at komme videre

--- MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/MainActivity.java
+++ MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/MainActivity.java
@@ -8,9 +8,23 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button main;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        main = (Button) findViewById(R.id.main);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Front_page.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
