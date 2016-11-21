f3376c85b1f9c6e8448d8341d5dd512042e3968f
Daniel Hansen <danielhansen1203@msn.com>
2016-11-21 13:51:24 +0100

    Toolbar implementeret

--- MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/Front_page.java
+++ MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/Front_page.java
@@ -3,9 +3,13 @@ package com.example.daniel.madpakkenproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Front_page extends AppCompatActivity {
 
@@ -45,5 +49,35 @@ public class Front_page extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater mMenuinflater = getMenuInflater();
+        mMenuinflater.inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.action_pay) {
+            Intent intent = new Intent(Front_page.this, PayActivity.class);
+            startActivity(intent);
+        }
+        if(item.getItemId() == R.id.action_profil) {
+            Intent intent = new Intent(Front_page.this, Profile_page.class);
+            startActivity(intent);
+        }
+        if(item.getItemId() == R.id.action_about_us) {
+            Intent intent = new Intent(Front_page.this, AboutUs.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
