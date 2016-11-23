8935f7d31fc3fddb65eb2b7fdb367c6eca4a1cb0
Daniel Hansen <danielhansen1203@msn.com>
2016-11-23 16:13:47 +0100

    Implementering af PayPal

--- MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/PayActivity.java
+++ MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/PayActivity.java
@@ -1,13 +1,80 @@
 package com.example.daniel.madpakkenproject;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import java.math.BigDecimal;
 
 public class PayActivity extends AppCompatActivity {
+
+    TextView m_response;
+
+    PayPalConfiguration m_configuration;
+    String m_paypalClientID = "AQ0Zhk2BvZfTJliMIB4TmB0kw9Krhjl5sCyZwiuCs3RAyyiTJd_gPOcVwyRdyNDKiYdzDwgNBOAUgFHv";
+    Intent m_service;
+    int m_paypalRequestCode = 999;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay);
+
+        m_response = (TextView) findViewById(R.id.response);
+
+        m_configuration = new PayPalConfiguration()
+                .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+                .clientId(m_paypalClientID);
+
+        m_service = new Intent(this, PayPalService.class);
+        m_service.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, m_configuration);
+        startService(m_service);
+    }
+
+    void pay(View view)
+    {
+        PayPalPayment payment= new PayPalPayment(new BigDecimal(10), "DKK", "Samlet pris fra Madpakken", PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, m_configuration);
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment);
+        startActivityForResult(intent, m_paypalRequestCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == m_paypalRequestCode)
+        {
+            if(resultCode == Activity.RESULT_OK);
+            {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+
+                if(confirmation != null)
+                {
+                    String state = confirmation.getProofOfPayment().getState();
+
+                    if(state.equals("approved"))
+                        m_response.setText("Betalingen er nu gennemført");
+
+                    else
+                        m_response.setText("Fejl i betalingen");
+                }
+                else
+                    m_response.setText("Kunne ikke bekræfte");
+            }
+        }
     }
 }
