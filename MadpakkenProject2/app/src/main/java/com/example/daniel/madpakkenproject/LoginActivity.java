01cd0c38021d40d06da291e20d7184ee5cbdb2fb
sayig86 <sayig86@gmail.com>
2016-11-29 11:39:17 +0100

    loginP

--- MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/LoginActivity.java
+++ MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.daniel.madpakkenproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,6 +15,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button buttonReg = (Button) findViewById(R.id.buttonReg);
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), RegisterActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
 
     }
 }
--- MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/MainActivity.java
+++ MadpakkenProject2/app/src/main/java/com/example/daniel/madpakkenproject/MainActivity.java
@@ -1,38 +1,36 @@
 package com.example.daniel.madpakkenproject;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
 
-    Button buttonl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonl = (Button) findViewById(R.id.buttonl);
-        buttonl.setOnClickListener(this);
+
+        Button buttonl = (Button) findViewById(R.id.buttonl);
+        buttonl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(v.getContext(), LoginActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
 
     }
 
 
-    private void buttonlClick()
-    {
-        startActivity(new Intent("com.example.daniel.madpakkenproject.LoginActivity"));
-}
-    @Override
-    public void onClick(View v) {
-        switch (v.getId())
-        {
-            case R.id.buttonl:
-                buttonlClick();
-                break;
-        }
-    }
+
+
 }
